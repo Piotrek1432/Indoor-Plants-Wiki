84289e0414d2828ab0aa4f1e25baebddf9af0d49
piotrjankowskicm <piotrjankowskicm@gmail.com>
2022-10-09 22:16:32 +0200

    init commit

---
+++ src/main/java/com/piotrjankowski/polsl/indoor_plants_wiki/IndoorPlantsWikiApplication.java
@@ -0,0 +1,13 @@
+package com.piotrjankowski.polsl.indoor_plants_wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorPlantsWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorPlantsWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piotrjankowski/polsl/indoor_plants_wiki/IndoorPlantsWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrjankowski.polsl.indoor_plants_wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoorPlantsWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
