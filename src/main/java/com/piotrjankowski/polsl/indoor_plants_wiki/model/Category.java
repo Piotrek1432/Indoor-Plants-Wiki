bf93be88c9c3fde4db596ae11ac36868cfb53c00
piotrjankowskicm <piotrjankowskicm@gmail.com>
2022-10-29 15:56:57 +0200

    Adding a "Category" entity

---
+++ src/main/java/com/piotrjankowski/polsl/indoor_plants_wiki/model/Category.java
@@ -0,0 +1,46 @@
+package com.piotrjankowski.polsl.indoor_plants_wiki.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    @NotBlank(message = "Category name cannot be empty")
+    private String name;
+    private String description;
+    @Embedded
+    private Audit audit = new Audit();
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void updateFrom(final Category toUpdate){
+        name = toUpdate.name;
+        description = toUpdate.description;
+    }
+}
